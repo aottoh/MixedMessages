7040ad2aef7d76fe890d4e5c58f774b3097510c0
Alexander Hoffmann <alex.otto.hoffmann@gmail.com>
2021-05-01 01:56:18 +0200

    Work of first night with a project outline to work on

---
+++ Scribbles.js
@@ -0,0 +1,13 @@
+
+// Working code example for user input
+// For my code I will include an 'exit' code word to exit the console input (readline.close()?)
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+   
+  readline.question('Who are you?', name => {
+    console.log(`Hey there ${name}!`);
+    readline.close();
+  });
---
+++ script.js
@@ -0,0 +1,123 @@
+/*
+Filename: script.js
+Project: MixedMessages
+Author: Alexander Hoffmann
+Version: 0.1.1
+Date: 2021-05-01
+*/
+
+// Creating the master array 
+
+let arrAllElements = [
+    {
+        _name: 'Augsburg',
+        _type: 'city',
+        wasPlayed: false,
+    },
+    {
+        _name: 'Austria',
+        _type: 'country',
+        wasPlayed: false,
+    },
+    {
+        _name: 'Amazon',
+        _type: 'river',
+        wasPlayed: false,
+    },
+    {
+        _name: 'Essen',
+        _type: 'city',
+        wasPlayed: false,
+    },
+    {
+        _name: 'Estonia',
+        _type: 'country',
+        wasPlayed: false,
+    },
+    {
+        _name: 'Elb',
+        _type: 'river',
+        wasPlayed: false,
+    },
+    {
+        _name: 'Ingolstadt',
+        _type: 'city',
+        wasPlayed: false,
+    },
+    {
+        _name: 'India',
+        _type: 'country',
+        wasPlayed: false,
+    },
+    {
+        _name: 'Inn',
+        _type: 'river',
+        wasPlayed: false,
+    },
+    {
+        _name: 'Oslo',
+        _type: 'city',
+        wasPlayed: false,
+    },
+    {
+        _name: 'Oman',
+        _type: 'country',
+        wasPlayed: false,
+    },
+    {
+        _name: 'Ohio River',
+        _type: 'river',
+        wasPlayed: false,
+    },
+    {
+        _name: 'Udaipur',
+        _type: 'city',
+        wasPlayed: false,
+    },
+    {
+        _name: 'Uzbekistan',
+        _type: 'country',
+        wasPlayed: false,
+    },
+    {
+        _name: 'Union River',
+        _type: 'river',
+        wasPlayed: false,
+    },
+    {
+        _name: 'Germersheim',
+        _type: 'city',
+        wasPlayed: false,
+    },
+    {
+        _name: 'Germany',
+        _type: 'country',
+        wasPlayed: false,
+    },
+    {
+        _name: 'Ganges',
+        _type: 'river',
+        wasPlayed: false,
+    }
+];
+
+
+
+// Declaring required functions
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+
+
+
+
+// Getting user input and running the algo
+
+
+
+// Testing the code
+//console.log(arrAllElements);
+console.log('Yay, did it work?: ' + userInput);
